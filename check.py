0116033d9cc106427da012ea1a1bc8d7ffb8c9dc
mdonze <mdonze@gmail.com>
2019-10-13 13:38:57 -0700

    Add files via upload

---
+++ check.py
@@ -0,0 +1,35 @@
+# This is a check/review to make sure nothing was "lost" over break
+# Name:
+# Period:
+
+# Variable declaration and assignment
+# Example
+# Check
+
+# While loops
+# Example
+# Check
+
+# String concatenation
+# Example
+# Check
+
+# input
+# Example
+# Check
+
+# casting
+# Example
+# Check
+
+# Functions
+# Example
+# Check
+
+# if/else statements
+# Example
+# Check
+
+# Create a random number
+# Example
+# Check
