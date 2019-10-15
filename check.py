fe1429450f3979faf29e26c3cdc22832b775a780
alxhubb <56038650+alxhubb@users.noreply.github.com>
2019-10-15 08:42:46 -0700

    Add files via upload

--- check.py
+++ check.py
@@ -1,35 +1,80 @@
 # This is a check/review to make sure nothing was "lost" over break
-# Name:
-# Period:
 
-# Variable declaration and assignment
-# Example
-# Check
 
-# While loops
-# Example
-# Check
+# Alex Hubbard
+# Period 1
 
-# String concatenation
+# Variable delaration and assignment
 # Example
-# Check
+myVar = "hello"
+# You try, delcare two variables 1 string and 1 number, assign values
+number = 1
+string = "yeet"
 
-# input
-# Example
-# Check
+# while loop
+# example
+x = 10
+while x > 0:
+	print(x)
+	x = x - 1
 
-# casting
-# Example
-# Check
 
-# Functions
-# Example
-# Check
 
-# if/else statements
-# Example
-# Check
+# you try, print your name 100 times
 
-# Create a random number
-# Example
-# Check
+name = input("What is your name?")
+while x < 100:
+	print(name)
+	x = x + 1
+
+# Don't do this
+#   print("Alex" * 100)
+
+
+# string concatenation
+# exampple
+name = "Alex"
+print("Hello " + name)
+# make a variable with your favorite movie
+# print "my favorite movie is"
+favMovie = "The Hitman's Bodyguard"
+print("My favorite movie is " + favMovie)
+
+# input 
+# example 
+myName = input("What is your name: ")
+print("Your name is: " + myName)
+
+# promt for favorite song and print "Your favorite song is"
+song = input("What is your favorite song?")
+# casting: changing the type of variable
+Num = 40
+print("My number is " + str(Num))
+num1 = input("enter a number: ")
+num1 = int(num1) + 10
+print("num1 + 10 is " + str(num1))
+
+# ask for two numbers, add thyem and print the answer
+var1 = input("Give me a number: ")
+var2 = input("give me another number: ")
+var3 = int(var1) + int(var2)
+print("Your number is " + str(var3))
+
+# if/else
+# example
+number = int(input(" Type a number "))
+
+if number > 100:
+	print("Your number is more than 100")
+elif number == 100:
+	print("Your number is equal to 100")
+else:
+	print("your number is less than 100")
+
+
+# ask if today is your birthday, if it is print happy birthday
+bday = input("Is today your birthday?")
+if bday == "yes":
+	print("Happy Birthday!!")
+else:
+	print("have a good day")
